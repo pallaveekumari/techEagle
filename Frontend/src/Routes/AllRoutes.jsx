eea8723bad314c577354a324da7b08ba34ed7bf5
pallaveekumari <pallavee.kumari@razorpay.com>
2023-12-14 12:30:04 +0530

    working-on-functionalities

--- Frontend/src/Routes/AllRoutes.jsx
+++ Frontend/src/Routes/AllRoutes.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './HomePage/HomePage'
+import Login from './Login/Login'
+import Signup from './Signup/Signup'
+import CartPage from './CartPage/CartPage'
 
 const AllRoutes = () => {
   return (
     <Routes>
 <Route path={"/"} element={<HomePage/>}/>
+<Route path={"/login"} element={<Login/>}/>
+<Route path={"signup"} element={<Signup/>}/>
+<Route path={"/cart"} element={<CartPage/>}/>
+
     </Routes>
   )
 }
