6b565f51531deb2b837916162313f9e52db93af3
pallaveekumari <pallavee.kumari@razorpay.com>
2023-12-14 15:54:58 +0530

    working-on-functionalities

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import AllRoutes from './Routes/AllRoutes';
 import NavBar from './Components/NavBar/NavBar';
+import Signup from './Routes/Signup/Signup';
+import EachCard from './Routes/EachCard/EachCard';
 
 function App() {
   return (
   //  <AllRoutes/>
-  <NavBar/>
+  // <NavBar/>
+  // <Signup/>
+  <EachCard/>
   );
 }
 
--- Frontend/src/Routes/Signup/Signup.jsx
+++ Frontend/src/Routes/Signup/Signup.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import styles from "../Signup/Signup.module.css"
+import { Box, Button, TextField } from '@mui/material'
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <Box className={styles.container}>
+        <Box className={styles.firstcontainer}>
+            Looks Like You're New here !
+        </Box>
+        <Box className={styles.secondcontainer}>
+            <TextField placeholder="Enter Name" type="text" />
+            <TextField placeholder="Enter Email/Phone Number" type="text" />
+            <TextField placeholder="Enter Address" type="text" />
+            <TextField placeholder="Enter Password" type="password" />
+        <Button variant='contained'>Submit</Button>
+        <Button variant='contained'>Login</Button>
+        </Box>
+        
+    </Box>
   )
 }
 
